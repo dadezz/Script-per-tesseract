07d1db329cd8c30f753ffa82a47f977a1a114295
davide zambon <dadezz@protonmail.com>
2022-04-19 13:30:27 +0200

    Update ossessione.bat

--- ossessione.bat
+++ ossessione.bat
@@ -1,5 +1,11 @@
-for /L %%i IN (1,1,116) DO (
-	tesseract C:\Users\Dade\Desktop\ossessione\ossessione_%%i.tif C:\Users\Dade\Desktop\ossessione\ossessione_%%i.txt
-	copy C:\Users\Dade\Desktop\ossessione\oss.txt+C:\Users\Dade\Desktop\ossessione\ossessione_%%i.txt.txt C:\Users\Dade\Desktop\ossessione\oss.txt
-	del C:\Users\Dade\Desktop\ossessione\ossessione_%%i.txt /q
+goto :start
+(questo è un commento)
+"n" nel for è il numero di foto presenti. queste foto sono salvate come nomefoto_1, nomefoto_2 etc
+è essenziale mettere il doppio ".txt" dove indicato, perché tesseract salva il file con il primo ".txt" come parte del nome, non come estensione
+:start
+
+for /L %%i IN (1,1,n) DO (
+	tesseract C:[percorso]\nomefoto_%%i.tif C:[percorso]\nomefoto__%%i.txt
+	copy C:[percorso]\filefinale.txt+C:[percorso]\nomefoto_%%i.txt.txt C:[percorso]\filefinale.txt
+	del C:[percorso]\nomefoto_%%i.txt.txt /q
 	)
